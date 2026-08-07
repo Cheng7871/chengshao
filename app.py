17442d8c19164c8c98350aeaa8551a04601883f6
Cheng7871 <2042264067@qq.com>
2026-08-07 10:53:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, request
-
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    real_ip = request.headers.get('X-Forwarded-For', request.remote_addr)
-    return f"访客IP：{real_ip}"
+def home():
+    visitor_ip = request.headers.get("X-Forwarded-For",request.remote_addr)
+    with open("index.html","r",encoding="utf-8")as file:
+        html_content = file.read()
+    html_content = html_content.replace("{{ip}}",visitor_ip)
+    return html_content
 
-if __name__ == "__main__":
+if __name__=="__main__":
     app.run(host="0.0.0.0")

